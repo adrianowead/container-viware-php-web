9aa13f21cc5ef6d7e47dde141c7a811db17b74d1
Adriano Maciel <adriano_mail@hotmail.com>
2022-02-24 17:23:33 -0300

    Fixada versao da imagem

--- publish.sh
+++ publish.sh
@@ -5,12 +5,13 @@ cd "$(dirname "$0")"
 
 export DOCKR_IMG_NAME=viware-base-php
 export DOCKR_HUB_REPO=adrianowead
+export DOCKR_IMG_VERSION=1.0.0
 
 # docker build
-docker build -t "$DOCKR_IMG_NAME" --file=./Dockerfile .
+docker build -t "$DOCKR_IMG_NAME:$DOCKR_IMG_VERSION" --file=./Dockerfile .
 
 # definir tag
-export DOCKR_IMG_NAME_TAGGED="$DOCKR_IMG_NAME:latest"
+export DOCKR_IMG_NAME_TAGGED="$DOCKR_IMG_NAME:$DOCKR_IMG_VERSION"
 export DOCKR_IMG_URI="$DOCKR_HUB_REPO/$DOCKR_IMG_NAME_TAGGED"
 
 docker tag "$DOCKR_IMG_NAME_TAGGED" "$DOCKR_IMG_URI"
